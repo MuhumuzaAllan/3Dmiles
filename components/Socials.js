30cbf1932fbf1a2038c5a49b78c37a2492395447
MuhumuzaAllan <muhumuzaallan09@gmail.com>
2024-05-05 10:43:55 +0000

    third chance is the charm

--- components/Socials.js
+++ components/Socials.js
@@ -6,12 +6,12 @@ import {RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiBehanceLine, RiPinter
 
 const Socials = () => {
   return <div className='flex items-center gap-x-5 text-lg' >
-    <Link href={''} className='hover:text-accent transition-all duration-300 ' > <RiYoutubeLine /> </Link>
-    <Link href={''} className='hover:text-accent transition-all duration-300 ' > <RiFacebookLine /> </Link>
+    <Link href={'https://www.youtube.com/@muhumuzaallan7915'} className='hover:text-accent transition-all duration-300 ' > <RiYoutubeLine /> </Link>
+    <Link href={'https://www.facebook.com/profile.php?id=61552736139715'} className='hover:text-accent transition-all duration-300 ' > <RiFacebookLine /> </Link>
     <Link href={''} className='hover:text-accent transition-all duration-300 ' > <RiInstagramLine /> </Link>
     <Link href={''} className='hover:text-accent transition-all duration-300 ' > <RiDribbbleLine /> </Link>
     <Link href={''} className='hover:text-accent transition-all duration-300 ' > <RiBehanceLine /> </Link>
-    <Link href={''} className='hover:text-accent transition-all duration-300 ' > <RiPinterestLine /> </Link>
+    <Link href={'https://www.pinterest.com/mileslegend/'} className='hover:text-accent transition-all duration-300 ' > <RiPinterestLine /> </Link>
   </div>;
 };
 
